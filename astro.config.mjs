ee7c8ac8d25567cc55af1f2c472fbdf488e3f442
Pavin Pankajan <pavinpankajan@Pavins-MacBook-Air.local>
2025-07-28 20:14:42 +0530

    Configure sampadavr.com as primary domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://web.sampadavr.com',
+  site: 'https://sampadavr.com',
   build: {
     assets: 'assets'
   }
