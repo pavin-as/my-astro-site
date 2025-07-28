a365c03d743acc5822b3255cef6fc2df02b1d1b8
Pavin Pankajan <pavinpankajan@Pavins-MacBook-Air.local>
2025-07-28 20:09:30 +0530

    Revert to GitHub Pages default URL to fix domain conflict

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://sampadavr.com', // Your custom domain
+  site: 'https://pavin-as.github.io',
+  base: '/my-astro-site',
   build: {
     assets: 'assets'
   }
