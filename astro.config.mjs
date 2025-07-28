ba8892486b80f453fedea028678cda8a4801cca7
Pavin Pankajan <pavinpankajan@Pavins-MacBook-Air.local>
2025-07-28 17:58:17 +0530

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://yourusername.github.io', // Replace with your GitHub username
+  site: 'https://pavin-as.github.io', // Replace with your GitHub username
   base: '/my-astro-site', // Replace with your repository name
   build: {
     assets: 'assets'
