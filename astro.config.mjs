1dcef350f9edbc6bdba9609facd02a1bbaaf36ed
Pavin Pankajan <pavinpankajan@Pavins-MacBook-Air.local>
2025-07-29 13:53:55 +0530

    Fix GitHub username in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://pavinpankajan.github.io',
+  site: 'https://pavin-as.github.io',
   base: '/my-astro-site/',
   build: {
     assets: 'assets'
